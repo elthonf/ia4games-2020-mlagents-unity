321995965cd079f307dd9bb9a01c68a88060e0dc
Elthon Manhas de Freitas <elthonf@gmail.com>
2020-06-30 15:03:17 -0300

    Preparação de Assets para a aula

---
+++ Assets/Scripts/MeuScript-MLAgent.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+
+public class MyAgent : Agent
+{
+    Rigidbody rBody;
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+    }
+
+    // Variáveis auxiliares expostas (Parametros)
+    public Transform Target;
+    public float speed = 10;
+
+    /// <summary>
+    /// Função que será chamada toda vez que precisar resetar a cena de treinamento
+    /// </summary>
+    public override void OnEpisodeBegin()
+    {
+       // Colocar aqui código para início
+    }
+
+    /// <summary>
+    /// Função que obtém os parâmetros de entrada da rede neural
+    /// </summary>
+    /// <param name="sensor"></param>
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        // Colocar aqui código para os "sensores"
+    }
+
+    public override void OnActionReceived(float[] vectorAction)
+    {
+        // Colocar aqui o código para "fazer algo"
+    }
+
+    public override void Heuristic(float[] actionsOut)
+    {
+        //Colocar aqui possíveis códigos que alterem a variável "actionsOut"
+    }
+}
